e76d7ec85ae5cf141fbcd036a1a643b1a638c090
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-10-18 21:55:54 +0900

    urls.py 수정

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('billing.urls'))
+    url(r'^iamport_test/', include('billing.urls'))
 ]
