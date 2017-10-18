7e7d7e492840eb72172a0c07b86c0c24d56833d3
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-10-18 19:55:30 +0900

    urls.py 작성

---
+++ django_app/billing/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+from .views import charge_point, PointCheckoutAjaxView, PointImpAjaxView
+
+urlpatterns = [
+    url(r'^charge/$', charge_point()),
+    url(r'^checkout/$', PointCheckoutAjaxView.as_view(), name='point_checkout'),
+    url(r'^validation/$', PointImpAjaxView.as_view(), name='point_validation'),
+]
