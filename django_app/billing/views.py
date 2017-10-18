aa35c4e2318e19caea5f7bf2985fd3cf8556e5c7
jeongmyeonghyeon <joengmyeonghyeon@gmail.com>
2017-10-18 19:46:53 +0900

    billing/views.py 작성

--- django_app/billing/views.py
+++ django_app/billing/views.py
@@ -1,3 +1,72 @@
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from .models import PointTransaction
+
+
+class PointCheckoutAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return JsonResponse({}, status=101)
+
+        user = request.user
+        amount = request.POST.get('amount')
+        type = request.POST.get('type')
+
+        try:
+            trnas = PointTransaction.objects.create_new(
+                user=user,
+                amount=amount,
+                type=type
+            )
+        except:
+            trans = None
+
+        if trans is not None:
+            data = {
+                "works": True,
+                "merchant_id": trans
+            }
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+
+class PointImpAjaxView(View):
+    def post(self, request, *args, **kwargs):
+        if not request.user.is_authenticated():
+            return JsonResponse({}, status=401)
+
+        user = request.user
+        merchant_id = request.POST.get('merchant_id')
+        imp_id = request.POST.get('imp_id')
+        amount = request.POST.get('amount')
+
+        try:
+            trnas = PointTransaction.objects.create_new(
+                user=user,
+                amount=amount,
+                type=type
+            )
+        except:
+            trans = None
+
+        if trans is not None:
+            trans.transaction_id = imp_id
+            trans.success = True
+            trans.save()
+
+            data = {
+                "works": True
+            }
+
+            return JsonResponse(data)
+        else:
+            return JsonResponse({}, status=401)
+
+
+def charge_point(request):
+    template = 'charge.html'
+
+    return render(request, template)
